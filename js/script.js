4180ca2cdc38c1c8ce24c4cafecb8c29da942ad5
Def <deferenseo@gmail.com>
2022-09-10 18:57:16 +0300

    started about block(video)

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ $(document).ready(function() {
         autoplayTimeout: 3000
     });
 });
+
+const videoBtn = document.querySelector('#video-btn');
+const videoPic = document.querySelector('.video__picture');
+const videoWrapper = document.querySelector('.video');
+
+videoBtn.addEventListener('click', () => {
+    videoPic.classList.add('none');
+    videoWrapper.classList.remove('video-overlay');
+});
