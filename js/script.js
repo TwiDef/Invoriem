2b61ba83c34db001df98660ad78d7043302baca8
Def <deferenseo@gmail.com>
2022-09-10 17:57:07 +0300

    finished slider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        dots: false,
+        margin: 48,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000
+    });
+});
