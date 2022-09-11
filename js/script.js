130d4c8e8aa55ef8c0d20d6b53edcd58b9e5a130
Def <deferenseo@gmail.com>
2022-09-11 14:09:28 +0300

    finished video block

--- js/script.js
+++ js/script.js
@@ -11,8 +11,19 @@ $(document).ready(function() {
 const videoBtn = document.querySelector('#video-btn');
 const videoPic = document.querySelector('.video__picture');
 const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
 
-videoBtn.addEventListener('click', () => {
-    videoPic.classList.add('none');
-    videoWrapper.classList.remove('video-overlay');
+
+videoWrapper.addEventListener('click', () => {
+    if (video.paused) {
+        videoPic.classList.add('none');
+        videoWrapper.classList.remove('video-overlay');
+        videoBtn.classList.add('none');
+        video.play();
+    } else {
+        videoWrapper.classList.add('video-overlay');
+        videoBtn.classList.remove('none');
+        video.pause();
+    }
+
 });
